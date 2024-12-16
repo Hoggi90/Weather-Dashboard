a2e8f0d9f153a7e3c5be7c51b54d628621ad4a2c
Hoger <hogga1@gmail.com>
2024-12-16 19:04:18 +0000

    testing ci/cd workflow

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__, static_folder='../frontend', static_url_path='/')
 
